3320af920ffe6075279c5caf7b2c77bb6585c1b3
Dana Ottaviani <dana.ottaviani@gmail.com>
2016-07-23 14:52:32 -0400

    added bCard helpers and expressions

---
+++ client/bCard/bCard.js
@@ -0,0 +1,20 @@
+Template.bCard.helpers({
+
+	firstName: function() {
+		return Session.get('firstName');
+	},
+
+	lastName: function() {
+		return Session.get('lastName');
+	},
+
+	quote: function() {
+		return Session.get('quote');
+	},
+
+	githubLink: function() {
+		return Session.get('githubLink');
+	}
+
+
+});
